307e0b59b04bed4b70bc60d882f9926fb19daf83
obaed <obaedul@redlimesolutions.com>
2024-07-04 13:08:43 +0600

    Home page design Done

--- src/app/site/home/home.component.ts
+++ src/app/site/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../components/footer/footer.component';
 import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styles: ``,
+  imports: [HeaderComponent, FooterComponent],
 })
 export class HomeComponent {}
