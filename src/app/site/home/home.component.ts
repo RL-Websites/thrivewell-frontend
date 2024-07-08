1bd5f08a5faa7b1c58c2bfb1c85c8ed616985f30
obaed <obaedul@redlimesolutions.com>
2024-07-08 10:45:15 +0600

    Done all tunning in ABout us page

--- src/app/site/home/home.component.ts
+++ src/app/site/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FooterComponent } from '../components/footer/footer.component';
 import { HeaderComponent } from '../components/header/header.component';
 
@@ -7,6 +8,6 @@ import { HeaderComponent } from '../components/header/header.component';
   standalone: true,
   templateUrl: './home.component.html',
   styles: ``,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
 })
 export class HomeComponent {}
