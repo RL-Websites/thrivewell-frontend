966c209ea5f7e2c97ae8733b0587975a6ba85907
obaed <obaedul@redlimesolutions.com>
2024-07-09 09:15:39 +0600

    Legitscript page design done

--- src/app/site/legit-script/legit-script.component.ts
+++ src/app/site/legit-script/legit-script.component.ts
@@ -1,43 +1,58 @@
-import { Component } from '@angular/core';
-import Swiper from 'swiper';
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Autoplay, Navigation } from 'swiper/modules';
+import { SwiperContainer } from 'swiper/element';
+import { register as swiperRegister } from 'swiper/element/bundle';
 import { FooterComponent } from '../components/footer/footer.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { OtherBannerFrameComponent } from '../components/other-banner-frame/other-banner-frame.component';
 
-Swiper.use([Autoplay, Navigation]);
-
+swiperRegister();
 @Component({
   selector: 'app-legit-script',
   standalone: true,
   templateUrl: './legit-script.component.html',
   styles: ``,
-  imports: [HeaderComponent, FooterComponent, OtherBannerFrameComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    OtherBannerFrameComponent,
+    CommonModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class LegitScriptComponent {
-  ngAfterViewInit(): void {
-    new Swiper('.client-slide', {
-      spaceBetween: 30,
-      loop: true,
-      centeredSlides: true,
+export class LegitScriptComponent implements AfterViewInit {
+  @ViewChild('clientSlider')
+  clientSlider: ElementRef<SwiperContainer>;
 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1.3,
-          spaceBetween: 15,
-        },
-        576: {
-          slidesPerView: 3.4,
-          spaceBetween: 50,
-        },
-      },
-    });
+  ngAfterViewInit(): void {}
+  breakpoints = {
+    0: {
+      slidesPerView: 1,
+    },
+    940: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+    1652: {
+      slidesPerView: 4,
+    },
+  };
+
+  prevSlide() {
+    this.clientSlider?.nativeElement?.swiper?.slidePrev();
+  }
+  nextSlide() {
+    this.clientSlider?.nativeElement?.swiper?.slideNext();
   }
 }
