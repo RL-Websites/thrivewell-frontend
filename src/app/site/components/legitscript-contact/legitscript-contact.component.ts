66baaf0cbf28bfc10d8665b6764d88607e13093f
obaed <obaedul@redlimesolutions.com>
2024-07-10 09:14:58 +0600

    some tunning done

---
+++ src/app/site/components/legitscript-contact/legitscript-contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'legitscript-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './legitscript-contact.component.html',
+  styles: ``,
+})
+export class LegitscriptContactComponent {}
