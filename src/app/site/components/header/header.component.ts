4a31378c9061f870afc6559632fc07b4d81c0ee7
obaed <obaedul@redlimesolutions.com>
2024-07-29 17:41:32 +0600

    DOne all tunning

--- src/app/site/components/header/header.component.ts
+++ src/app/site/components/header/header.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  OnDestroy,
+  PLATFORM_ID,
+  Renderer2,
+} from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 import AOS from 'aos';
@@ -11,14 +18,35 @@ import AOS from 'aos';
   templateUrl: './header.component.html',
   styles: ``,
 })
-export class HeaderComponent implements AfterViewInit {
+export class HeaderComponent implements AfterViewInit, OnDestroy {
   routerLink: any;
   public isCollapsed = true;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private renderer: Renderer2
+  ) {}
+
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       AOS.init();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.renderer.setStyle(document.body, 'overflow', 'auto');
+    }
+  }
+
+  toggleNavbar() {
+    this.isCollapsed = !this.isCollapsed;
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.isCollapsed) {
+        this.renderer.setStyle(document.body, 'overflow', 'auto');
+      } else {
+        this.renderer.setStyle(document.body, 'overflow', 'hidden');
+      }
     }
   }
 }
