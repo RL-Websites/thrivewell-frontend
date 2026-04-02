3923ffcfa5e7bf5407254490e32a30b18e239173
Nestor Ramos <nramos@redlimesolutions.com>
2026-04-02 10:53:13 -0500

    feat(product-flow): remove phone field and wire up demo form submission

--- src/app/site/product-flow/product-flow.component.ts
+++ src/app/site/product-flow/product-flow.component.ts
@@ -5,6 +5,8 @@ import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { OtherBannerFrameComponent } from '../components/other-banner-frame/other-banner-frame.component';
+import { ContactService } from '@app/services/contact.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-product-flow',
@@ -27,11 +29,13 @@ export class ProductFlowComponent implements OnInit, OnDestroy {
     name: '',
     email: '',
     company: '',
-    phone: '',
     message: '',
   };
 
-  constructor(@Inject(DOCUMENT) private readonly document: Document) {}
+  constructor(
+    @Inject(DOCUMENT) private readonly document: Document,
+    private contactService: ContactService
+  ) {}
 
   ngOnInit(): void {
     this.document.body.classList.add(ProductFlowComponent.bodyClass);
@@ -45,5 +49,58 @@ export class ProductFlowComponent implements OnInit, OnDestroy {
 
   onDemoSubmit(event: Event): void {
     event.preventDefault();
+
+    const payload = {
+      first_name: this.demoForm.name,
+      last_name: '',
+      email: this.demoForm.email,
+      phone_number: '',
+      company: this.demoForm.company,
+      notes: this.demoForm.message,
+    };
+
+    this.contactService.createBooking(payload).subscribe({
+      next: () => {
+        Swal.fire({
+          customClass: { confirmButton: 'btn-cta' },
+          html: `
+            <div class="pt-30 px-30">
+              <img src="images/modal/thumbs-up.svg">
+              <h3 class="fs-md-43 fs-30 fw-normal text-dark mt-20">Thank You</h3>
+              <p class="fs-md-18 fs-16 fw-medium text-gray mb-0">Your details have been successfully submitted.</p>
+              <div class="modal-frame">
+                <div class="frame-top-left">
+                  <img class="position-absolute start-0 top-0" src="images/modal/top-left.svg">
+                </div>
+                <div class="frame-bottom-right">
+                  <img class="position-absolute end-0 bottom-0" src="images/modal/bottom-right.svg">
+                </div>
+              </div>
+            </div>`,
+          confirmButtonText: 'Okay',
+        });
+        this.demoForm = { name: '', email: '', company: '', message: '' };
+      },
+      error: () => {
+        Swal.fire({
+          customClass: { confirmButton: 'btn-cta' },
+          html: `
+            <div class="pt-30 px-30">
+              <img src="images/modal/error.svg">
+              <h3 class="fs-md-43 fs-30 fw-normal text-dark mt-20">Sorry</h3>
+              <p class="fs-md-18 fs-16 fw-medium text-gray mb-0 mt-20">Your details could not be submitted.</p>
+              <div class="modal-frame">
+                <div class="frame-top-left">
+                  <img class="position-absolute start-0 top-0" src="images/modal/top-left.svg">
+                </div>
+                <div class="frame-bottom-right">
+                  <img class="position-absolute end-0 bottom-0" src="images/modal/bottom-right.svg">
+                </div>
+              </div>
+            </div>`,
+          confirmButtonText: 'Okay',
+        });
+      },
+    });
   }
 }
